5e4d8276244972579d30d594e5ea5c44adf0d1f1
HarshitPatel25 <harshitcode25@gmail.com>
2020-05-26 15:54:43 +0530

    added first GET Route

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+    {name:'Harshit'},
+    {name:'patel'}
+]
+
+app.get('/user',function(){
+    res.json({
+      sucess:true,
+      message:'sucessfully got user... Nice!!',
+      users: mockUserData
+    })
+})
+
+
 app.listen(8000,function(){
     console.log("Server is running")
 })
